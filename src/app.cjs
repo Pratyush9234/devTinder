cd729ce9775e5464f89b94e1fcc91fcead4810d9
Pratyush Kumar <pratyush93041kumarsinha@gmail.com>
2025-10-02 22:48:40 +0530

    uses of respone handlers and middlewares

--- src/app.cjs
+++ src/app.cjs
@@ -4,16 +4,54 @@ const app=express();
 
 //handling the code 
 
-app.use("/test",(req,res)=>{
-    res.send("Hello from the test");  //request handler
+// app.get("/user/:userID/:name/:password",(req,res)=>{
+//    console.log(req.params);
+//    res.send({firstname:"pratyush",lastname:"Kumar"});
+// });
+
+// app.post("/dashboard",(req,res)=>{
+//   res.send("Hello im responding from dashboard");
+// });
+
+// app.post("/serach",(req,res)=>{
+//   res.send({Firstname:"Aayush",lastname:"Kumar"});
+// });
+
+// app.use("/route",(req,res)=>{
+//   res.send("jai hind jai bharat");
+// });
+
+// app.use("/test",(req,res)=>{
+//     res.send("Hello from the test");  //request handler
+// });
+
+// app.use("/hello",(req,res)=>{
+//    res.send("Hello im responding");
+// });
+
+// app.use("/dashboard",(req,res)=>{
+//   res.send("Hello from the dashboard to the client");
+// });
+
+app.use("/admin",((req,res,next)=>{
+  console.log("Admin authentication is getting checked ");
+  const token="xyz";
+  const isAuthorized= token==="xyz";
+  if(!isAuthorized){
+    res.status(401).send("The admin is unauthorized");
+  }
+  else{
+    next();                                                 // this whole section is the middleware as it prevents
+                                                            // from writing the logic again and again 
+  }
+}));
+
+app.use("/admin/getAlldata",(req,res)=>{
+   res.send("The admin is authorized and all data is sent ");
 });
 
-app.use("/hello",(req,res)=>{
-   res.send("Hello im responding");
-});
-
-app.use("/dashboard",(req,res)=>{
-  res.send("Hello from the dashboard to the client");
+app.use("/admin/DeleteAlldata",(req,res)=>{
+  res.send("Unauthorized access and data is not sent");
 });
 
 app.listen(7777,()=>{
